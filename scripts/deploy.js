6202ff40c4f4726cb76c254e153732077cabf10b
siddharth-nain <124575984+siddharth-nain@users.noreply.github.com>
2024-10-02 23:38:25 +0530

    Update deploy.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -7,13 +7,16 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Voting = await hre.ethers.getContractFactory("Voting");
-  const voting = await Voting.deploy();
-  await voting.deployed();
+  const initBalance = 1;
+  const Assessment = await hre.ethers.getContractFactory("Assessment");
+  const assessment = await Assessment.deploy(initBalance);
+  await assessment.deployed();
 
-  console.log(`Voting contract deployed to ${voting.address}`);
+  console.log(`A contract with balance of ${initBalance} eth deployed to ${assessment.address}`);
 }
 
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
